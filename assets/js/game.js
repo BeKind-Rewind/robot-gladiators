1cff66aada790b710615b1909e1ff7ca7d8f9d52
BeKind-Rewind <challenge641@gmail.com>
2022-06-11 11:50:13 -0500

    added console.logs to script

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,12 @@
-window.prompt("What is your robot's name?");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string");
+console.log(10+10);
+console.log("our robot name is " + playerName);
+
 
 function fight() {
     window.alert("The fight has begun!");
 }
 
-fight();
+// fight();
