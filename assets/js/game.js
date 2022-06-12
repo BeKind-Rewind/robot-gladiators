f9fa7573533b95f4f0d3b4f4f3397798d9011cf8
BeKind-Rewind <challenge641@gmail.com>
2022-06-11 22:42:34 -0500

    setting up MVP

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,31 +8,45 @@ var enemyAttack = 12;
 
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
+    if (promptFight === "fight" || promptFight === "FIGHT");
     enemyHealth = enemyHealth - playerAttack;
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
     if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
-    }
-    else {
+    } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
+}
 
-    playerHealth = playerHealth - enemyAttack;
-    console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
+    //     
+    // enemyHealth = enemyHealth - playerAttack;
+//     console.log(
+//         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+//     );
+//     if (enemyHealth <= 0) {
+//         window.alert(enemyName + " has died!");
+//     }
+//     else {
+//         window.alert(enemyName + " still has " + enemyHealth + " health left.");
+//     }
 
-    if(playerHealth <= 0) {
-        window.alert(playerName + " has died!");
-    }
-    else {
-        window.alert(playerName + " still has " + playerHealth + " health left.");
-    }
+//     playerHealth = playerHealth - enemyAttack;
+//     console.log(
+//         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+//     );
+
+//     if(playerHealth <= 0) {
+//         window.alert(playerName + " has died!");
+//     }
+//     else {
+//         window.alert(playerName + " still has " + playerHealth + " health left.");
+//     }
 
 
-};
+// };
 
 
 fight();
