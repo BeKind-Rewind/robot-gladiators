4da19bd5f46789658b09c2b35c734be268d329ec
BeKind-Rewind <challenge641@gmail.com>
2022-06-16 16:22:25 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -40,8 +40,11 @@ var fight = function(enemyName) {
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = Math.max(0, enemyHealth - playerAttack);
-      console.log(
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+
+        enemyHealth = Math.max(0, enemyHealth - damage);
+        
+        console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
   
@@ -58,8 +61,11 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = Math.max(0, playerHealth - enemyAttack);
-      console.log(
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+        playerHealth = Math.max(0, playerHealth - damage);
+      
+        console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
   
@@ -92,7 +98,7 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
     
       // reset enemyHealth before starting new fight
-      enemyHealth = randomNumber();
+      enemyHealth = randomNumber(40, 60);
     
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
